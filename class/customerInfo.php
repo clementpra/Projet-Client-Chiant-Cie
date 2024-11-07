2238c4bd418e122be4744ac3c56450904f3efb97
Clément <92472229+clementpra@users.noreply.github.com>
2024-11-07 12:25:44 +0000

    another test

--- class/customerInfo.php
+++ class/customerInfo.php
@@ -7,7 +7,7 @@ class customerInfo{
     function getCustomerInfo(){
         $mysqlClient = PDOMySQLConnector::getClient();
 
-        $requeteSQL = 'SELECT * FROM customersInfo';
+        $requeteSQL = 'SELECT * FROM \'customersInfo\'';
         $requete = $mysqlClient->prepare($requeteSQL);
         $requete->execute();
         $result = $requete->fetchAll();
