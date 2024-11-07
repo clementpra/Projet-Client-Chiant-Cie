161a830c615375eef991087fc33464b953a4eef4
Clément <92472229+clementpra@users.noreply.github.com>
2024-11-07 12:27:39 +0000

    .

--- api.php
+++ api.php
@@ -2,9 +2,9 @@
 
 $mysqlClient = new PDO('mysql:host=10.20.9.130;dbname=ProjetClientChiantDB;charset=utf8', 'root', '8331chrC');
 
-$requeteSQL = 'SELECT * FROM customersInfo';
-$requete = $mysqlClient->prepare($requeteSQL);
-$requete->execute();
-$result = $requete->fetchAll();
+//$requeteSQL = 'SELECT * FROM customersInfo';
+//$requete = $mysqlClient->prepare($requeteSQL);
+//$requete->execute();
+//$result = $requete->fetchAll();
 
-echo $result;
+//echo $result;
