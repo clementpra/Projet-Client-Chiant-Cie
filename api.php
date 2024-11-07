8d5e8f57b868cf71a7994aae58a84b087b479b7f
Clément <92472229+clementpra@users.noreply.github.com>
2024-11-07 12:39:04 +0000

    Test n°5468

--- api.php
+++ api.php
@@ -1,10 +1,10 @@
 <?php
-echo "...";
-//$mysqlClient = new PDO('mysql:host=10.20.9.130;dbname=ProjetClientChiantDB;charset=utf8', 'root', '8331chrC');
 
-//$requeteSQL = 'SELECT * FROM customersInfo';
-//$requete = $mysqlClient->prepare($requeteSQL);
-//$requete->execute();
-//$result = $requete->fetchAll();
+$mysqlClient = new PDO('mysql:host=10.20.9.130;dbname=ProjetClientChiantDB;charset=utf8', 'root', '8331chrC');
 
-//echo $result;
+$requeteSQL = 'SELECT * FROM customersInfo';
+$requete = $mysqlClient->prepare($requeteSQL);
+$requete->execute();
+$result = $requete->fetchAll();
+
+echo $result;
