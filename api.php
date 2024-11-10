da00271b432cec107e17e841b5dfe386bffebc99
Clément <92472229+clementpra@users.noreply.github.com>
2024-11-10 09:36:27 +0000

    test api.php

--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php
 
-$mysqlClient = new PDO('mysql:host=10.20.9.130;dbname=ProjetClientChiantDB;charset=utf8', 'root', '8331chrC');
+$mysqlClient = new PDO('mysql:host=10.20.9.131;dbname=ProjetClientChiantDB;charset=utf8', 'webserv', '{)9YbiIuM.e!7z(');
 
-$requeteSQL = 'SELECT * FROM customersInfo';
+$requeteSQL = 'SELECT * FROM CustomersInfo';
 $requete = $mysqlClient->prepare($requeteSQL);
 $requete->execute();
 $result = $requete->fetchAll();
