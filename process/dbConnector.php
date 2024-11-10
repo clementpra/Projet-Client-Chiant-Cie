cf0c72cc3ab061225b1bdadab3bcdcd39945b86d
Clément <92472229+clementpra@users.noreply.github.com>
2024-11-10 09:35:36 +0000

    change db password...

--- process/dbConnector.php
+++ process/dbConnector.php
@@ -7,7 +7,7 @@ class PDOMySQLConnector
     public static function getClient()
     {
         if (self::$mysqlClient == null) {
-            self::$mysqlClient = new PDO('mysql:host=10.20.9.131;dbname=ProjetClientChiantDB;charset=utf8', 'root', '8331chrC');
+            self::$mysqlClient = new PDO('mysql:host=10.20.9.131;dbname=ProjetClientChiantDB;charset=utf8', 'webserv', '{)9YbiIuM.e!7z(');
         }
         return self::$mysqlClient;
     }
